52219edb80f37a21254584117cfe4ab97af6e85b
Kevin Leneschmidt <kevin@gates-app.de>
2024-08-23 13:11:56 +0200

    new init function

---
+++ src/services/scandit-scanner.ts
@@ -0,0 +1,146 @@
+import 'scandit-capacitor-datacapture-core'
+import 'scandit-capacitor-datacapture-barcode'
+
+import { Device } from '@capacitor/device'
+import {
+  Camera,
+  DataCaptureContext,
+  DataCaptureView,
+  FrameSourceState,
+  LaserlineViewfinder,
+  LaserlineViewfinderStyle,
+  MarginsWithUnit,
+  MeasureUnit,
+  NumberWithUnit,
+  ScanditCaptureCorePlugin,
+  ScanIntention,
+} from 'scandit-capacitor-datacapture-core'
+import {
+  BarcodeCapture,
+  BarcodeCaptureListener,
+  BarcodeCaptureOverlay,
+  BarcodeCaptureOverlayStyle,
+  BarcodeCaptureSettings,
+  BatterySavingMode,
+  Symbology,
+} from 'scandit-capacitor-datacapture-barcode'
+
+//TODO: add keys here
+const IOS_SCANDIT_KEY =
+  ''
+const ANDROID_SCANDIT_KEY =
+  ''
+
+class ScanditScanner {
+  private initialized = false
+  private context: DataCaptureContext | null = null
+  private settings: BarcodeCaptureSettings | null = null
+  private barcodeCapture: BarcodeCapture | null = null
+  private view: DataCaptureView | null = null
+  private listener: BarcodeCaptureListener | null = null
+  private camera: Camera | null = null
+  private overlay: BarcodeCaptureOverlay | null = null
+
+  private getKey = async () => {
+    const info = await Device.getInfo()
+    if (info.platform === 'ios') {
+      return IOS_SCANDIT_KEY
+    } else {
+      return ANDROID_SCANDIT_KEY
+    }
+  }
+
+  private init = async () => {
+    if (!this.initialized) {
+      await ScanditCaptureCorePlugin.initializePlugins()
+
+      this.initialized = true
+
+      const key = await this.getKey()
+
+      this.context = DataCaptureContext.forLicenseKey(key)
+
+      this.camera = Camera.default
+      this.context.setFrameSource(this.camera)
+
+      this.settings = new BarcodeCaptureSettings()
+
+      this.settings.enableSymbologies([
+        Symbology.EAN13UPCA,
+        Symbology.EAN8,
+        Symbology.UPCE,
+        Symbology.Code128,
+        Symbology.InterleavedTwoOfFive,
+        Symbology.QR,
+      ])
+
+      this.settings.batterySavingMode = BatterySavingMode.Auto
+      this.settings.scanIntention = ScanIntention.Smart
+
+      const symbologySettings = this.settings.settingsForSymbology(Symbology.Code39)
+      symbologySettings.activeSymbolCounts = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+      this.barcodeCapture = BarcodeCapture.forContext(this.context, this.settings)
+    }
+    this.view = DataCaptureView.forContext(this.context)
+    this.view.connectToElement(document.getElementById('dataCaptureView')!)
+
+    this.view!.scanAreaMargins = new MarginsWithUnit(
+      new NumberWithUnit(0, MeasureUnit.DIP),
+      new NumberWithUnit(0, MeasureUnit.DIP),
+      new NumberWithUnit(100, MeasureUnit.DIP),
+      new NumberWithUnit(100, MeasureUnit.DIP)
+    )
+
+    this.overlay = BarcodeCaptureOverlay.withBarcodeCaptureForViewWithStyle(
+      this.barcodeCapture!,
+      this.view,
+      BarcodeCaptureOverlayStyle.Frame
+    )
+
+    this.overlay.viewfinder = new LaserlineViewfinder(LaserlineViewfinderStyle.Legacy)
+
+    this.camera?.switchToDesiredState(FrameSourceState.On)
+    this.barcodeCapture!.isEnabled = true
+  }
+
+  public startScanner = async (newListener: BarcodeCaptureListener) => {
+    this.listener = newListener
+    await this.init()
+    this.barcodeCapture?.addListener(this.listener)
+    this.resumeScanner()
+  }
+
+  public stopScanner = () => {
+    if (this.listener) {
+      this.barcodeCapture?.removeListener(this.listener)
+      this.listener = null
+    }
+    this.view?.detachFromElement()
+    this.camera?.switchToDesiredState(FrameSourceState.Off)
+  }
+
+  public pauseScanner = () => {
+    if (this.barcodeCapture) {
+      this.barcodeCapture.isEnabled = false
+    }
+  }
+
+  public resumeScanner = () => {
+    if (this.barcodeCapture) {
+      this.barcodeCapture.isEnabled = true
+    }
+  }
+
+  public hideScanner = () => {
+    const element = document.getElementById('dataCaptureView')
+    if (element) element.style.display = 'none'
+  }
+
+  public showScanner = () => {
+    const element = document.getElementById('dataCaptureView')
+    if (element) element.style.display = 'block'
+  }
+}
+
+export const scanditScanner = new ScanditScanner()
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,55 +2,104 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Scandit Scanner Example</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-label color="danger"
+        >Issue: Multiple Listeners are getting added after closing and starting the scanner again.</ion-label
+      >
+      <p>
+        Each time you restart the scanner, an additional toast notification (or listener?) is added to the screen after
+        scanning a barcode. This can be observed by dismissing the toast or checking the console logs.
+      </p>
+      <p>
+        Another issue is, that "Capacitor plugin "ScanditBarcodeNative" already registered. Cannot register plugins
+        twice." is getting shown in the console, but I dont know if it is only in the web.
+      </p>
+      <p style="font-weight: 600">
+        To reproduce the issue, add your keys in the "src/services/scandit-scanner.ts" file, build the app (i used iOS),
+        click on the "Start Scanner" button, scan a barcode, and then click on the "Stop Scanner" button. Repeat this
+        process a few times to see, that every time one more toast is getting shown.
+      </p>
+      <div v-if="showScanner" class="scanner-container">
+        <div id="dataCaptureView" class="scanner-content" />
       </div>
     </ion-content>
+    <ion-footer>
+      <ion-button @click="handleScanner" expand="full" :color="showScanner ? 'danger' : undefined" class="button">
+        {{ showScanner ? 'Stop Scanner' : 'Start Scanner' }}
+      </ion-button>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue'
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonFooter,
+  toastController,
+  IonButton,
+} from '@ionic/vue'
+import { BarcodeCapture, BarcodeCaptureSession } from 'scandit-capacitor-datacapture-barcode'
+
+import { scanditScanner } from '@/services/scandit-scanner'
+
+const showScanner = ref(false)
+
+const scannerListener = {
+  didScan: async (barcodeCapture: BarcodeCapture, session: BarcodeCaptureSession) => {
+    const barcode = session.newlyRecognizedBarcodes[0].data?.trim() || ''
+    console.log('🚀 ~ didScan: ~ barcode:', barcode)
+    scanditScanner.pauseScanner()
+    const toast = await toastController.create({
+      message: `Barcode: ${barcode} scanned!`,
+      duration: 2000,
+      swipeGesture: 'vertical',
+      position: 'top',
+    })
+    await toast.present()
+    setTimeout(async () => {
+      await toast.dismiss()
+      scanditScanner.resumeScanner()
+    }, 2000)
+  },
+}
+
+const handleScanner = () => {
+  if (showScanner.value) {
+    scanditScanner.stopScanner()
+    showScanner.value = false
+  } else {
+    showScanner.value = true
+    scanditScanner.startScanner(scannerListener)
+  }
+}
 </script>
-
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.scanner-container {
+  position: relative;
+  height: 250px;
+  width: 100%;
+  background: transparent !important;
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.scanner-content {
+  width: 100%;
+  height: 100%;
+  background-color: transparent !important;
+  --background: transparent !important;
 }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
+.button {
+  height: 4rem;
   margin: 0;
-}
-
-#container a {
-  text-decoration: none;
 }
 </style>
